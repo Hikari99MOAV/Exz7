1368359fc8a969d1682cc3eba77e24cc194efe77
Hikari99MOAV <78300258+Hikari99MOAV@users.noreply.github.com>
2021-06-27 21:57:35 +0300

    Add files via upload

---
+++ Exz7/Exz7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exz7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("максимальное значение для двух различных вещественных чисел");
+            Console.Write("Введите первое число ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double max;
+            if (a > b)
+            {
+                max = a;
+
+            }
+            else
+            {
+                max = b;
+            }
+            Console.WriteLine("макс число :{0}", max);
+        }
+    }
+}
